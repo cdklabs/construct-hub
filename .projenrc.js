752bf0290ddc2874b3996935cf321978a9d9b53a
👨🏻‍💻 Romain Marcadier <rmuller@amazon.com>
2021-05-05 09:11:50 +0200

    chore: prepare publishing information

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,4 @@
-const { readdirSync, statSync, existsSync } = require('fs');
-const { join } = require('path');
-const { AwsCdkConstructLibrary, SourceCode, FileBase } = require('projen');
+const { AwsCdkConstructLibrary } = require('projen');
 
 const project = new AwsCdkConstructLibrary({
   jsiiFqn: 'projen.AwsCdkConstructLibrary',
@@ -18,7 +16,7 @@ const project = new AwsCdkConstructLibrary({
   dependabot: false,
 
   author: 'Amazon Web Services, Inc.',
-  authorAddress: 'aws-cdk-team@amazon.com',
+  authorAddress: 'construct-ecosystem-team@amazon.com',
   authorOrganization: true,
 
   cdkVersion: '1.101.0',
@@ -54,11 +52,17 @@ const project = new AwsCdkConstructLibrary({
     '*By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*',
   ],
 
-  releaseToNpm: true,
-  npmRegistryUrl: 'https://npm.pkg.github.com/',
-  npmTokenSecret: 'GITHUB_TOKEN',
-
   projenUpgradeSecret: 'CDK_AUTOMATION_GITHUB_TOKEN',
+
+  releaseToNpm: true,
+
+  publishToGo: {},
+  publishToMaven: {},
+  publishToNuget: {},
+  publishToPypi: {
+    distName: 'construct-hub',
+    module: 'construct_hub',
+  },
 });
 
 function addDevApp() {
