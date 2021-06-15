f41e92cdacf4468b2d3d2d9751a58600e2cd743c
Romain Marcadier <romain.muller@telecomnancy.net>
2021-06-15 15:42:59 +0200

    feat: catalog builder function (#78)
    This function can be invoked periodically or through an event to
    re-build the `catalog.json` object in the package store S3 bucket. It
    can either work incrementally (making changes to the existing object),
    or from scratch (re-building the catalog object from the complete list
    of indexed packages as found in the S3 bucket, which may be slower).
    
    Fixes #6

--- src/backend/transliterator/index.ts
+++ src/backend/transliterator/index.ts
@@ -6,7 +6,7 @@ import { Construct, Duration } from '@aws-cdk/core';
 import { Transliterator as Handler } from './transliterator';
 
 export interface TransliteratorProps {
-  readonly sourceBucket: Bucket;
+  readonly bucket: Bucket;
 
   /**
    * How long should execution logs be retained?
@@ -22,16 +22,18 @@ export class Transliterator extends Construct {
 
     const lambda = new Handler(this, 'Default', {
       deadLetterQueueEnabled: true,
+      description: 'Creates transliterated assemblies from jsii-enabled npm packages',
+      logRetention: props.logRetention ?? RetentionDays.TEN_YEARS,
       memorySize: 10_240, // Currently the maximum possible setting
       retryAttempts: 2,
       timeout: Duration.minutes(15),
     });
 
     // The handler reads & writes to this bucket.
-    props.sourceBucket.grantReadWrite(lambda);
+    props.bucket.grantReadWrite(lambda);
 
     // Creating the event chaining
-    lambda.addEventSource(new S3EventSource(props.sourceBucket, {
+    lambda.addEventSource(new S3EventSource(props.bucket, {
       events: [EventType.OBJECT_CREATED],
       filters: [{ prefix: 'packages/', suffix: '/package.tgz' }],
     }));
