a6549d9b976546d7f22ef1670268272c23e2527f
iliapolo <epolon@amazon.com>
2021-06-28 15:35:53 +0200

    build

--- src/__tests__/construct-hub.test.ts
+++ src/__tests__/construct-hub.test.ts
@@ -28,7 +28,7 @@ test('with domain', () => {
 
   const cert = new DnsValidatedCertificate(stack, 'Cert', { hostedZone: zone, domainName: zone.zoneName });
 
-  new ConstructHub(stack, 'ConstructHub', {
+  new ConstructHub(stack, 'ConstructBBBHub', {
     domain: {
       zone, cert,
     },
