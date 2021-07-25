04c1b87fe956b88229e73832ee3eb941bbb29824
Romain Marcadier <romain.muller@telecomnancy.net>
2021-07-25 17:37:36 +0200

    chore: add button to reprocess all function on dashboard (#217)
    This makes it easy-access.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- src/backend/orchestration/index.ts
+++ src/backend/orchestration/index.ts
@@ -35,6 +35,12 @@ export class Orchestration extends Construct {
    */
   public readonly redriveFunction: IFunction;
 
+  /**
+   * The function operators can use to reprocess all indexed packages through
+   * the backend data pipeline.
+   */
+  public readonly reprocessAllFunction: IFunction;
+
   public constructor(scope: Construct, id: string, props: OrchestrationProps) {
     super(scope, id);
 
@@ -103,7 +109,7 @@ export class Orchestration extends Construct {
 
     // This function is intended to be manually triggered by an operator to
     // reprocess all package versions currently in store through the back-end.
-    const reprocessAll = new ReprocessAll(this, 'ReprocessAll', {
+    this.reprocessAllFunction = new ReprocessAll(this, 'ReprocessAll', {
       description: '[ConstructHub/ReprocessAll] Reprocess all package versions through the backend',
       environment: {
         BUCKET_NAME: props.bucket.bucketName,
@@ -112,7 +118,7 @@ export class Orchestration extends Construct {
       memorySize: 1_024,
       timeout: Duration.minutes(15),
     });
-    props.bucket.grantRead(reprocessAll);
-    this.stateMachine.grantStartExecution(reprocessAll);
+    props.bucket.grantRead(this.reprocessAllFunction);
+    this.stateMachine.grantStartExecution(this.reprocessAllFunction);
   }
 }
