cdklabs/construct-hub

commit cf575b0ceff5af3ff04f426e3f56a2ca4e7a175c

feat: allow configuring multiple package sources (#300)
Replaced the `Discovery` function with a pluggable `IPackageSource` API,
and providing an `NpmJs` as well as `CodeArtifact` source. These sources
can optionally be passed into the `ConstructHub` constructor to register
alternate package sources (by default, an `NpmJs` source is registered).

This unfortunately involved relativelu wide-spread changes in the
codebase, as some features had to be made visible from the public API of
the module, and some code needed to be re-used (hence, refactored to a
different location).

Fixes #281


----

*By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

src/package-sources/npmjs.ts
@@ -1,0 +1,272 @@
+import { ComparisonOperator, GraphWidget, Metric, MetricOptions, Statistic, TreatMissingData } from '@aws-cdk/aws-cloudwatch';
+import { Rule, Schedule } from '@aws-cdk/aws-events';
+import { LambdaFunction } from '@aws-cdk/aws-events-targets';
+import { Tracing } from '@aws-cdk/aws-lambda';
+import { BlockPublicAccess, Bucket, IBucket } from '@aws-cdk/aws-s3';
+import { Construct, Duration } from '@aws-cdk/core';
+import { lambdaFunctionUrl, s3ObjectUrl } from '../deep-link';
+import { fillMetric } from '../metric-utils';
+import type { IPackageSource, PackageSourceBindOptions, PackageSourceBindResult } from '../package-source';
+import { MARKER_FILE_NAME, METRICS_NAMESPACE, MetricName, S3KeyPrefix } from './npmjs/constants.lambda-shared';
+
+import { NpmJsFollower } from './npmjs/npm-js-follower';
+
+export interface NpmJsProps {
+  /**
+   * The bucket to use for staging npm packages.
+   *
+   * @default - a new bucket will be created.
+   */
+  readonly stagingBucket?: IBucket;
+}
+
+/**
+ * A package source that gets package data from the npmjs.com package registry.
+ */
+export class NpmJs implements IPackageSource {
+  public constructor(private readonly props: NpmJsProps = {}) {}
+
+  public bind(
+    scope: Construct,
+    { denyList, ingestion, licenseList, monitoring, queue, repository }: PackageSourceBindOptions,
+  ): PackageSourceBindResult {
+    repository?.addExternalConnection('public:npmjs');
+
+    const bucket = this.props.stagingBucket || new Bucket(scope, 'NpmJs/StagingBucket', {
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      enforceSSL: true,
+      lifecycleRules: [{ prefix: S3KeyPrefix.STAGED_KEY_PREFIX, expiration: Duration.days(30) }],
+    });
+    bucket.grantRead(ingestion);
+
+    const follower = new NpmJsFollower(scope, 'NpmJs', {
+      description: `[${scope.node.path}/NpmJs] Periodically query npmjs.com index for new packages`,
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+        QUEUE_URL: queue.queueUrl,
+      },
+      memorySize: 10_024, // 10 GiB
+      reservedConcurrentExecutions: 1, // Only one execution at a time, to avoid race conditions on the S3 marker object
+      timeout: Duration.minutes(5),
+      tracing: Tracing.ACTIVE,
+    });
+
+    bucket.grantReadWrite(follower);
+    queue.grantSendMessages(follower);
+    denyList?.grantRead(follower);
+    licenseList.grantRead(follower);
+
+    const rule = new Rule(scope, 'NpmJs/Schedule', {
+      description: `${scope.node.path}/NpmJs/Schedule`,
+      schedule: Schedule.rate(Duration.minutes(5)),
+      targets: [new LambdaFunction(follower)],
+    });
+
+    const failureAlarm = follower.metricErrors().createAlarm(scope, 'NpmJs/Follower/Failures', {
+      alarmName: `${scope.node.path}/NpmJs/Follower/Failures`,
+      alarmDescription: [
+        'The NpmJs follower function failed!',
+        '',
+        `Direct link to Lambda function: ${lambdaFunctionUrl(follower)}`,
+      ].join('\n'),
+      comparisonOperator: ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+      evaluationPeriods: 3,
+      threshold: 1,
+      treatMissingData: TreatMissingData.MISSING,
+    });
+    monitoring.addHighSeverityAlarm('NpmJs/Follower Failures', failureAlarm);
+
+    const notRunningAlarm = follower.metricErrors().createAlarm(scope, 'NpmJs/Follower/NotRunning', {
+      alarmName: `${scope.node.path}/NpmJs/Follower/NotRunning`,
+      alarmDescription: [
+        'The NpmJs follower function is not running!',
+        '',
+        `Direct link to Lambda function: ${lambdaFunctionUrl(follower)}`,
+      ].join('\n'),
+      comparisonOperator: ComparisonOperator.LESS_THAN_THRESHOLD,
+      evaluationPeriods: 2,
+      threshold: 1,
+      treatMissingData: TreatMissingData.BREACHING,
+    });
+    monitoring.addHighSeverityAlarm('NpmJs/Follower Not Running', notRunningAlarm);
+
+    // Finally - the "not running" alarm depends on the schedule (it won't run until the schedule
+    // exists!), and the schedule depends on the failure alarm existing (we don't want it to run
+    // before we can know it is failing). This means the returned `IDependable` effectively ensures
+    // all alarms have been provisionned already! Isn't it nice!
+    notRunningAlarm.node.addDependency(rule);
+    rule.node.addDependency(failureAlarm);
+
+    return {
+      name: follower.node.path,
+      links: [
+        { name: 'NpmJs Follower', url: lambdaFunctionUrl(follower), primary: true },
+        { name: 'Marker Object', url: s3ObjectUrl(bucket, MARKER_FILE_NAME) },
+      ],
+      dashboardWidgets: [
+        [
+          new GraphWidget({
+            height: 6,
+            width: 12,
+            title: 'Function Health',
+            left: [
+              fillMetric(follower.metricInvocations({ label: 'Invocations' })),
+              fillMetric(follower.metricErrors({ label: 'Errors' })),
+            ],
+            leftYAxis: { min: 0 },
+            right: [
+              this.metricRemainingTime({ label: 'Remaining Time' }),
+            ],
+            rightYAxis: { min: 0 },
+            period: Duration.minutes(15),
+          }),
+          new GraphWidget({
+            height: 6,
+            width: 12,
+            title: 'CouchDB Follower',
+            left: [
+              this.metricChangeCount({ label: 'Change Count' }),
+              this.metricUnprocessableEntity({ label: 'Unprocessable' }),
+            ],
+            leftYAxis: { min: 0 },
+            right: [
+              fillMetric(this.metricNpmJsChangeAge({ label: 'Lag to npmjs.com' }), 'REPEAT'),
+              fillMetric(this.metricPackageVersionAge({ label: 'Package Version Age' }), 'REPEAT'),
+            ],
+            rightYAxis: { label: 'Milliseconds', min: 0, showUnits: false },
+            period: Duration.minutes(15),
+          }),
+        ],
+      ],
+    };
+  }
+
+  /**
+   * The average time it took to process a changes batch.
+   */
+  public metricBatchProcessingTime(opts?: MetricOptions): Metric {
+    return new Metric({
+      period: Duration.minutes(5),
+      statistic: Statistic.AVERAGE,
+      ...opts,
+      metricName: MetricName.BATCH_PROCESSING_TIME,
+      namespace: METRICS_NAMESPACE,
+    });
+  }
+
+  /**
+   * The total count of changes that were processed.
+   */
+  public metricChangeCount(opts?: MetricOptions): Metric {
+    return new Metric({
+      period: Duration.minutes(5),
+      statistic: Statistic.SUM,
+      ...opts,
+      metricName: MetricName.CHANGE_COUNT,
+      namespace: METRICS_NAMESPACE,
+    });
+  }
+
+  public metricNpmJsChangeAge(opts?: MetricOptions): Metric {
+    return new Metric({
+      period: Duration.minutes(5),
+      statistic: Statistic.MINIMUM,
+      ...opts,
+      metricName: MetricName.NPMJS_CHANGE_AGE,
+      namespace: METRICS_NAMESPACE,
+    });
+  }
+
+  /**
+   * The age of the oldest package version that was processed.
+   */
+  public metricPackageVersionAge(opts?: MetricOptions): Metric {
+    return new Metric({
+      period: Duration.minutes(5),
+      statistic: Statistic.MAXIMUM,
+      ...opts,
+      metricName: MetricName.PACKAGE_VERSION_AGE,
+      namespace: METRICS_NAMESPACE,
+    });
+  }
+
+  /**
+   * The total count of package versions that were inspected.
+   */
+  public metricPackageVersionCount(opts?: MetricOptions): Metric {
+    return new Metric({
+      period: Duration.minutes(5),
+      statistic: Statistic.SUM,
+      ...opts,
+      metricName: MetricName.PACKAGE_VERSION_COUNT,
+      namespace: METRICS_NAMESPACE,
+    });
+  }
+
+  /**
+   * The total count of package versions that were deemed relevant.
+   */
+  public metricRelevantPackageVersions(opts?: MetricOptions): Metric {
+    return new Metric({
+      period: Duration.minutes(5),
+      statistic: Statistic.SUM,
+      ...opts,
+      metricName: MetricName.RELEVANT_PACKAGE_VERSIONS,
+      namespace: METRICS_NAMESPACE,
+    });
+  }
+
+  /**
+   * The amount of time that was remaining when the lambda returned in order to
+   * avoid hitting a timeout.
+   */
+  public metricRemainingTime(opts?: MetricOptions): Metric {
+    return new Metric({
+      period: Duration.minutes(5),
+      statistic: Statistic.AVERAGE,
+      ...opts,
+      metricName: MetricName.REMAINING_TIME,
+      namespace: METRICS_NAMESPACE,
+    });
+  }
+
+  /**
+   * The total count of staging failures.
+   */
+  public metricStagingFailureCount(opts?: MetricOptions): Metric {
+    return new Metric({
+      period: Duration.minutes(5),
+      statistic: Statistic.SUM,
+      ...opts,
+      metricName: MetricName.STAGING_FAILURE_COUNT,
+      namespace: METRICS_NAMESPACE,
+    });
+  }
+
+  /**
+   * The average time it took to stage a package to S3.
+   */
+  public metricStagingTime(opts?: MetricOptions): Metric {
+    return new Metric({
+      period: Duration.minutes(5),
+      statistic: Statistic.AVERAGE,
+      ...opts,
+      metricName: MetricName.STAGING_TIME,
+      namespace: METRICS_NAMESPACE,
+    });
+  }
+
+  /**
+   * The amount of changes that were not processed due to having an invalid
+   * format.
+   */
+  public metricUnprocessableEntity(opts?: MetricOptions): Metric {
+    return new Metric({
+      period: Duration.minutes(5),
+      statistic: Statistic.SUM,
+      ...opts,
+      metricName: MetricName.UNPROCESSABLE_ENTITY,
+      namespace: METRICS_NAMESPACE,
+    });
+  }
+}

.gitattributes
@@ -25,7 +25,6 @@
 /src/backend/catalog-builder/catalog-builder.ts               	linguist-generated
 /src/backend/deny-list/prune-handler.ts                       	linguist-generated
 /src/backend/deny-list/prune-queue-handler.ts                 	linguist-generated
-/src/backend/discovery/discovery.ts                           	linguist-generated
 /src/backend/ingestion/ingestion.ts                           	linguist-generated
 /src/backend/inventory/canary.ts                              	linguist-generated
 /src/backend/orchestration/redrive-state-machine.ts           	linguist-generated
@@ -33,6 +32,8 @@
 /src/backend/transliterator/transliterator.ts                 	linguist-generated
 /src/monitored-certificate/certificate-monitor.ts             	linguist-generated
 /src/monitoring/http-get-function.ts                          	linguist-generated
+/src/package-sources/codeartifact/code-artifact-forwarder.ts  	linguist-generated
+/src/package-sources/npmjs/npm-js-follower.ts                 	linguist-generated
 /src/spdx-license.ts                                          	linguist-generated
 /src/webapp/cache-invalidator/handler.ts                      	linguist-generated
 /tsconfig.eslint.json                                         	linguist-generated

.gitignore
@@ -69,7 +69,6 @@ yarn-error.log*
 !/src/backend/catalog-builder/catalog-builder.ts
 !/src/backend/deny-list/prune-handler.ts
 !/src/backend/deny-list/prune-queue-handler.ts
-!/src/backend/discovery/discovery.ts
 !/src/backend/ingestion/ingestion.ts
 !/src/backend/inventory/canary.ts
 !/src/backend/orchestration/redrive-state-machine.ts
@@ -77,6 +76,8 @@ yarn-error.log*
 !/src/backend/transliterator/transliterator.ts
 !/src/monitored-certificate/certificate-monitor.ts
 !/src/monitoring/http-get-function.ts
+!/src/package-sources/codeartifact/code-artifact-forwarder.ts
+!/src/package-sources/npmjs/npm-js-follower.ts
 !/src/spdx-license.ts
 !/src/webapp/cache-invalidator/handler.ts
 !/tsconfig.eslint.json

src/__tests__/backend/catalog-builder/catalog-builder.lambda.test.ts
@@ -20,12 +20,12 @@ const MOCK_DENY_LIST_BUCKET = 'deny-list-bucket-name';
 const MOCK_DENY_LIST_OBJECT = 'my-deny-list.json';
 const MOCK_DENY_LIST_MAP: DenyListMap = {
   'name/v0.0.0-pre': {
-    package: 'name',
+    packageName: 'name',
     version: '0.0.0-pre',
     reason: 'blocked version',
   },
   '@foo/blocked': {
-    package: '@foo/blocked',
+    packageName: '@foo/blocked',
     reason: 'block all version of this package please',
   },
 };

src/__tests__/backend/deny-list/client.test.ts
@@ -7,12 +7,12 @@ import * as aws from '../../../backend/shared/aws.lambda-shared';
 
 const sample: Record<string, DenyListRule> = {
   'foo/v1.2.3': {
-    package: 'foo',
+    packageName: 'foo',
     version: '1.2.3',
     reason: 'bar',
   },
   'bar': {
-    package: 'bar',
+    packageName: 'bar',
     reason: 'hello bar',
   },
 };
@@ -89,7 +89,7 @@ describe('lookup', () => {
 
   test('match specific package + version', () => {
     expect(client.lookup('foo', '1.2.3')).toStrictEqual({
-      package: 'foo',
+      packageName: 'foo',
       version: '1.2.3',
       reason: 'bar',
     });
@@ -97,7 +97,7 @@ describe('lookup', () => {
 
   test('match any version', () => {
     const expected = {
-      package: 'bar',
+      packageName: 'bar',
       reason: 'hello bar',
     };
 

src/__tests__/backend/deny-list/deny-list.test.ts
@@ -76,35 +76,35 @@ describe('createDenyListMap()', () => {
 
   test('rule with "package"', () => {
     const rules: DenyListRule[] = [
-      { package: 'my-package', reason: 'my reason' },
+      { packageName: 'my-package', reason: 'my reason' },
     ];
     expect(createDenyListMap(rules)).toEqual({
-      'my-package': { package: 'my-package', reason: 'my reason' },
+      'my-package': { packageName: 'my-package', reason: 'my reason' },
     });
   });
 
   test('rule with scoped "package"', () => {
     const rules: DenyListRule[] = [
-      { package: '@my-scope/my-package', reason: 'my reason' },
+      { packageName: '@my-scope/my-package', reason: 'my reason' },
     ];
     expect(createDenyListMap(rules)).toEqual({
-      '@my-scope/my-package': { package: '@my-scope/my-package', reason: 'my reason' },
+      '@my-scope/my-package': { packageName: '@my-scope/my-package', reason: 'my reason' },
     });
   });
 
   test('rule with "package" and "version"', () => {
     const rules: DenyListRule[] = [
-      { package: 'my-package', version: '1.2.3', reason: 'my reason 1.2.3' },
+      { packageName: 'my-package', version: '1.2.3', reason: 'my reason 1.2.3' },
     ];
     expect(createDenyListMap(rules)).toEqual({
-      'my-package/v1.2.3': { package: 'my-package', version: '1.2.3', reason: 'my reason 1.2.3' },
+      'my-package/v1.2.3': { packageName: 'my-package', version: '1.2.3', reason: 'my reason 1.2.3' },
     });
   });
 
   test('fail for duplicate rules for the same package + version', () => {
     const rules: DenyListRule[] = [
-      { package: 'my-package', version: '1.2.3', reason: 'my reason 1.2.3' },
-      { package: 'my-package', version: '1.2.3', reason: 'your reason' },
+      { packageName: 'my-package', version: '1.2.3', reason: 'my reason 1.2.3' },
+      { packageName: 'my-package', version: '1.2.3', reason: 'your reason' },
     ];
 
     expect(() => createDenyListMap(rules)).toThrow(/Duplicate deny list entry: my-package\/v1\.2\.3/);
@@ -112,9 +112,9 @@ describe('createDenyListMap()', () => {
 
   test('fails for duplicate rules for p+v,p (in that order)', () => {
     const rules: DenyListRule[] = [
-      { package: 'my-package', version: '1.2.3', reason: 'only my-package@1.2.3 is blocked' },
-      { package: 'my-package', version: '3.4.5', reason: 'only my-package@3.4.5 is blocked' },
-      { package: 'my-package', reason: 'all versions of my-package are denied' },
+      { packageName: 'my-package', version: '1.2.3', reason: 'only my-package@1.2.3 is blocked' },
+      { packageName: 'my-package', version: '3.4.5', reason: 'only my-package@3.4.5 is blocked' },
+      { packageName: 'my-package', reason: 'all versions of my-package are denied' },
     ];
 
     expect(() => createDenyListMap(rules)).toThrow(/Found rules that match specific versions of \"my-package\" \(1\.2\.3,3\.4\.5\) but there is also a rule that matches all versions/);
@@ -122,9 +122,9 @@ describe('createDenyListMap()', () => {
 
   test('fails for duplicate rules for p,p+v,p+v (in that order)', () => {
     const rules: DenyListRule[] = [
-      { package: 'my-package', reason: 'all versions of my-package are denied' },
-      { package: 'my-package', version: '1.2.3', reason: 'only my-package@1.2.3 is blocked' },
-      { package: 'my-package', version: '3.4.5', reason: 'only my-package@3.4.5 is blocked' },
+      { packageName: 'my-package', reason: 'all versions of my-package are denied' },
+      { packageName: 'my-package', version: '1.2.3', reason: 'only my-package@1.2.3 is blocked' },
+      { packageName: 'my-package', version: '3.4.5', reason: 'only my-package@3.4.5 is blocked' },
     ];
 
     expect(() => createDenyListMap(rules)).toThrow(/Found rules that match specific versions of \"my-package\" \(1\.2\.3,3\.4\.5\) but there is also a rule that matches all versions/);
@@ -132,9 +132,9 @@ describe('createDenyListMap()', () => {
 
   test('fails for duplicate rules for p+v,p,p+v (in that order)', () => {
     const rules: DenyListRule[] = [
-      { package: 'my-package', version: '1.2.3', reason: 'only my-package@1.2.3 is blocked' },
-      { package: 'my-package', reason: 'all versions of my-package are denied' },
-      { package: 'my-package', version: '3.4.5', reason: 'only my-package@3.4.5 is blocked' },
+      { packageName: 'my-package', version: '1.2.3', reason: 'only my-package@1.2.3 is blocked' },
+      { packageName: 'my-package', reason: 'all versions of my-package are denied' },
+      { packageName: 'my-package', version: '3.4.5', reason: 'only my-package@3.4.5 is blocked' },
     ];
 
     expect(() => createDenyListMap(rules)).toThrow(/Found rules that match specific versions of \"my-package\" \(1\.2\.3,3\.4\.5\) but there is also a rule that matches all versions/);

commit 497e8d931b4cfc5d026f11dddf60374ff29a39be

fix: follower NotRunning alarm is misconfigured  (#312)
Bug introduced in https://github.com/cdklabs/construct-hub/pull/300 mistakenly used `metricErrors` to configure this alarm instead of `metricInvocations`. 

----

*By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

src/package-sources/npmjs.ts
@@ -76,7 +76,7 @@ export class NpmJs implements IPackageSource {
     });
     monitoring.addHighSeverityAlarm('NpmJs/Follower Failures', failureAlarm);
 
-    const notRunningAlarm = follower.metricErrors().createAlarm(scope, 'NpmJs/Follower/NotRunning', {
+    const notRunningAlarm = follower.metricInvocations().createAlarm(scope, 'NpmJs/Follower/NotRunning', {
       alarmName: `${scope.node.path}/NpmJs/Follower/NotRunning`,
       alarmDescription: [
         'The NpmJs follower function is not running!',

src/__tests__/__snapshots__/construct-hub.test.ts.snap
@@ -5953,7 +5953,7 @@ Direct link to Lambda function: /lambda/home#/functions/",
           },
         ],
         "EvaluationPeriods": 2,
-        "MetricName": "Errors",
+        "MetricName": "Invocations",
         "Namespace": "AWS/Lambda",
         "Period": 300,
         "Statistic": "Sum",
@@ -13345,7 +13345,7 @@ Direct link to Lambda function: /lambda/home#/functions/",
           },
         ],
         "EvaluationPeriods": 2,
-        "MetricName": "Errors",
+        "MetricName": "Invocations",
         "Namespace": "AWS/Lambda",
         "Period": 300,
         "Statistic": "Sum",
@@ -19784,7 +19784,7 @@ Direct link to Lambda function: /lambda/home#/functions/",
           },
         ],
         "EvaluationPeriods": 2,
-        "MetricName": "Errors",
+        "MetricName": "Invocations",
         "Namespace": "AWS/Lambda",
         "Period": 300,
         "Statistic": "Sum",

src/__tests__/devapp/__snapshots__/snapshot.test.ts.snap
@@ -3132,7 +3132,7 @@ Resources:
         - Name: FunctionName
           Value:
             Ref: ConstructHubSourcesNpmJs15A77D2D
-      MetricName: Errors
+      MetricName: Invocations
       Namespace: AWS/Lambda
       Period: 300
       Statistic: Sum