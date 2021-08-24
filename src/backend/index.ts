fa42355428b4062daacc4117b31be3abdc36e4cf
👨🏻‍💻 Romain Marcadier <rmuller@amazon.com>
2021-08-24 09:26:55 +0200

    remove spurious export

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,6 +1,4 @@
 export * from './catalog-builder';
-export * from '../package-sources/npmjs';
 export * from './ingestion';
 export * from './transliterator';
-export * from '../package-sources/npmjs';
 export * from './deny-list';
