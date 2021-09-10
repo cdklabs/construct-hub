e9be9ab005249c9bd1a4a932fe0b977057a6989d
Mitchell Valine <mitchellvaline@yahoo.com>
2021-09-10 13:53:58 -0700

    fix: broken release workflow snapshot tests (#323)
    The release workflow is currently broken because when running `build`
    after running `bump`, the snapshot content has changed due to the
    version number being included in links to the runbook.
    
    This changes the url to always reference the latest version.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- src/runbook-url.ts
+++ src/runbook-url.ts
@@ -1,9 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { version } = require('../package.json');
-
 /**
  * The URL to the runbook for this release of ConstructHub.
  */
-export const RUNBOOK_URL = version === '0.0.0'
-  ? 'https://github.com/cdklabs/construct-hub/blob/main/docs/operator-runbook.md'
-  : `https://github.com/cdklabs/construct-hub/blob/v${version}/docs/operator-runbook.md`;
+export const RUNBOOK_URL = 'https://github.com/cdklabs/construct-hub/blob/main/docs/operator-runbook.md';
