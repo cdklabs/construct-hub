ba14a0d629c5462cd3e37a6d6b7dd2bb60d0840c
Christopher Rybicki <rybickic@amazon.com>
2021-10-05 09:10:35 -0700

    feat: npm downloads client

---
+++ src/__tests__/backend/catalog-builder/npm-downloads.lambda-shared.test.ts
@@ -0,0 +1,162 @@
+import type { Got } from 'got';
+import { NpmDownloadsClient, NpmDownloadsPeriod } from '../../../backend/catalog-builder/npm-downloads.lambda-shared';
+
+jest.mock('got');
+
+const sampleData = {
+  'npm': {
+    downloads: 12345,
+    package: 'npm',
+    start: 'start-date',
+    end: 'end-date',
+  },
+  'express': {
+    downloads: 23456,
+    package: 'express',
+    start: 'start-date',
+    end: 'end-date',
+  },
+  'react': {
+    downloads: 34567,
+    package: 'react',
+    start: 'start-date',
+    end: 'end-date',
+  },
+  '@aws-cdk/core': {
+    downloads: 45678,
+    package: '@aws-cdk/core',
+    start: 'start-date',
+    end: 'end-date',
+  },
+  '@aws-cdk/aws-s3': {
+    downloads: 56789,
+    package: '@aws-cdk/aws-s3',
+    start: 'start-date',
+    end: 'end-date',
+  },
+};
+
+describe('getNpmDownloads', () => {
+  test('gets the total downloads of a package for the last day', async () => {
+    // GIVEN
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const fakeGot = require('got') as jest.MockedFunction<Got>;
+    fakeGot.mockImplementation(() => ({ body: JSON.stringify(sampleData.npm) }) as any);
+    const client = new NpmDownloadsClient(fakeGot);
+
+    // WHEN
+    const output = await client.getDownloads(['npm'], { period: NpmDownloadsPeriod.LAST_DAY });
+
+    // THEN
+    expect(output).toEqual({ npm: sampleData.npm });
+    expect(fakeGot).lastCalledWith(
+      `${NpmDownloadsClient.NPM_DOWNLOADS_API_URL}/last-day/npm`,
+      expect.anything(),
+    );
+  });
+
+  test('gets the total downloads of a package for the last week', async () => {
+    // GIVEN
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const fakeGot = require('got') as jest.MockedFunction<Got>;
+    fakeGot.mockImplementation(() => ({ body: JSON.stringify(sampleData.npm) }) as any);
+    const client = new NpmDownloadsClient(fakeGot);
+
+    // WHEN
+    const output = await client.getDownloads(['npm'], { period: NpmDownloadsPeriod.LAST_WEEK });
+
+    // THEN
+    expect(output).toEqual({ npm: sampleData.npm });
+    expect(fakeGot).lastCalledWith(
+      `${NpmDownloadsClient.NPM_DOWNLOADS_API_URL}/last-week/npm`,
+      expect.anything(),
+    );
+  });
+
+  test('gets the total downloads of a package for the last month', async () => {
+    // GIVEN
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const fakeGot = require('got') as jest.MockedFunction<Got>;
+    fakeGot.mockImplementation(() => ({ body: JSON.stringify(sampleData.npm) }) as any);
+    const client = new NpmDownloadsClient(fakeGot);
+
+    // WHEN
+    const output = await client.getDownloads(['npm'], { period: NpmDownloadsPeriod.LAST_MONTH });
+
+    // THEN
+    expect(output).toEqual({ npm: sampleData.npm });
+    expect(fakeGot).lastCalledWith(
+      `${NpmDownloadsClient.NPM_DOWNLOADS_API_URL}/last-month/npm`,
+      expect.anything(),
+    );
+  });
+
+  test('requests scoped packages in individual queries', async () => {
+    // GIVEN
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const fakeGot = require('got') as jest.MockedFunction<Got>;
+    fakeGot.mockImplementation(((url: string) => {
+      if (url.endsWith('/last-week/@aws-cdk/core')) {
+        return { body: JSON.stringify(sampleData['@aws-cdk/core']) };
+      } else if (url.endsWith('/last-week/@aws-cdk/aws-s3')) {
+        return { body: JSON.stringify(sampleData['@aws-cdk/aws-s3']) };
+      } else if (url.endsWith('/last-week/npm,express,react')) {
+        return {
+          body: JSON.stringify({
+            npm: sampleData.npm,
+            express: sampleData.express,
+            react: sampleData.react,
+          }),
+        };
+      } else {
+        throw new Error(`unexpected url: ${url}`);
+      }
+    }) as any);
+    const client = new NpmDownloadsClient(fakeGot);
+
+    // WHEN
+    const output = await client.getDownloads(['npm', 'express', 'react', '@aws-cdk/core', '@aws-cdk/aws-s3']);
+
+    // THEN
+    expect(output).toEqual({
+      '@aws-cdk/core': sampleData['@aws-cdk/core'],
+      '@aws-cdk/aws-s3': sampleData['@aws-cdk/aws-s3'],
+      'npm': sampleData.npm,
+      'express': sampleData.express,
+      'react': sampleData.react,
+    });
+  });
+
+  test('batches packages into bulk queries', async () => {
+    // GIVEN
+    const numUnscopedPackages = NpmDownloadsClient.MAX_PACKAGES_PER_QUERY + 5;
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const fakeGot = require('got') as jest.MockedFunction<Got>;
+    fakeGot.mockImplementation(((url: string) => {
+      if (url.endsWith('/last-week/' + Array(128).fill('express').join(','))) {
+        return { body: JSON.stringify(sampleData.express) };
+      } else if (url.endsWith('/last-week/' + Array(5).fill('express').join(','))) {
+        return { body: JSON.stringify(sampleData.express) };
+      } else if (url.endsWith('/last-week/@aws-cdk/core')) {
+        return { body: JSON.stringify(sampleData['@aws-cdk/core']) };
+      } else {
+        throw new Error(`unexpected url: ${url}`);
+      }
+    }) as any);
+    const client = new NpmDownloadsClient(fakeGot);
+
+    // WHEN
+    const packages = Array(numUnscopedPackages).fill('express');
+    packages.push(...Array(5).fill('@aws-cdk/core'));
+    const output = await client.getDownloads(packages);
+
+    // THEN
+    expect(output).toEqual({
+      '@aws-cdk/core': sampleData['@aws-cdk/core'],
+      'express': sampleData.express,
+    });
+    // called twice to bulk query the 128 + 5 unscoped packages,
+    // and five times to query each scoped package
+    expect(fakeGot).toHaveBeenCalledTimes(7);
+  });
+});
---
+++ src/backend/catalog-builder/npm-downloads.lambda-shared.ts
@@ -0,0 +1,112 @@
+import type { Got } from 'got/dist/source/types';
+
+export enum NpmDownloadsPeriod {
+  /**
+   * Gets downloads for the last available day. In practice, this will usually
+   * be "yesterday" (in GMT) but if stats for that day have not yet landed, it
+   * will be the day before.
+   */
+  LAST_DAY = 'last-day',
+
+  /**
+   * Gets downloads for the last 7 available days.
+   */
+  LAST_WEEK = 'last-week',
+
+  /**
+   * Gets downloads for the last 30 available days.
+   */
+  LAST_MONTH = 'last-month'
+}
+
+export interface NpmDownloadsEntry {
+  readonly downloads: number;
+  readonly start: string;
+  readonly end: string;
+  readonly package: string;
+}
+
+export type NpmDownloadsOutput = { [key: string]: NpmDownloadsEntry };
+
+export interface NpmDownloadsOptions {
+  /**
+   * The period to query for package download counts.
+   * @default NpmDownloadsPeriod.LAST_WEEK
+   */
+  readonly period?: NpmDownloadsPeriod;
+}
+
+export class NpmDownloadsClient {
+  public static readonly NPM_DOWNLOADS_API_URL = 'https://api.npmjs.org/downloads/point';
+  public static readonly MAX_PACKAGES_PER_QUERY = 128; // this is a limitation from npm's API! do not change
+
+  private readonly got: Got;
+
+  constructor(gotService: Got) {
+    this.got = gotService;
+  }
+
+  private async getDownloadsRaw(
+    packages: string[],
+    period: NpmDownloadsPeriod,
+  ): Promise<NpmDownloadsOutput> {
+    if (packages.length > NpmDownloadsClient.MAX_PACKAGES_PER_QUERY) {
+      throw new Error(`Too many packages were provided (max: ${NpmDownloadsClient.MAX_PACKAGES_PER_QUERY})`);
+    }
+    if (packages.length === 0) return {};
+
+    const result = await this.got(`${NpmDownloadsClient.NPM_DOWNLOADS_API_URL}/${period}/${packages.join(',')}`, {
+      timeout: 30 * 1000, // 30 seconds
+    });
+
+    const data = JSON.parse(result.body);
+    if (packages[0] in data) {
+      // multiple packages were returned
+      return data;
+    } else {
+      // only a single package was returned
+      return {
+        [packages[0]]: data,
+      };
+    }
+  }
+
+  /**
+   * Returns the number of downloads each package has on npm in the latest period.
+   * Output is not guaranteed to be returned in a specific order.
+   */
+  async getDownloads(
+    packages: string[],
+    options: NpmDownloadsOptions = {},
+  ): Promise<NpmDownloadsOutput> {
+    const period = options.period ?? NpmDownloadsPeriod.LAST_WEEK;
+
+    // separate scoped and unscoped packages since scoped packages are not
+    // supported by the bulk query API
+    const scopedPackages = [];
+    const unscopedPackages = [];
+    for (const pkg of packages) {
+      if (this.isScopedPackage(pkg)) {
+        scopedPackages.push(pkg);
+      } else {
+        unscopedPackages.push(pkg);
+      }
+    }
+
+    // we could parallelize this, but then it's more likely we get throttled
+    const output: NpmDownloadsOutput = {};
+    for (const pkg of scopedPackages) {
+      Object.assign(output, await this.getDownloadsRaw([pkg], period));
+    }
+    for (let i = 0; i < unscopedPackages.length; i += NpmDownloadsClient.MAX_PACKAGES_PER_QUERY) {
+      const batch = unscopedPackages.slice(i, i + NpmDownloadsClient.MAX_PACKAGES_PER_QUERY);
+      Object.assign(output, await this.getDownloadsRaw(batch, period));
+    }
+
+    return output;
+  }
+
+  private isScopedPackage(packageName: string) {
+    return packageName.indexOf('/') !== -1;
+  }
+}
