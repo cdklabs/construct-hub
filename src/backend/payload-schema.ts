a65bffe7e4273d6db656d50d8d7d0b480a4733aa
👨🏻‍💻 Romain Marcadier <rmuller@amazon.com>
2021-10-27 11:02:22 +0200

    feat: option to select languages through orchestration
    Adds an optional payload key to select which languages should be
    processed through ingestion. When not provided, the old approach
    of generating everything is used. When provided, only the enabled
    languages are generated.
    
    This will help work through language backfills faster.

--- src/backend/payload-schema.ts
+++ src/backend/payload-schema.ts
@@ -3,6 +3,20 @@ export interface StateMachineInput {
   readonly assembly: S3ObjectVersion;
   readonly package: S3ObjectVersion;
   readonly metadata: S3ObjectVersion;
+
+  /**
+   * If provided, limits which languages will actually be rendered.
+   *
+   * @default - all languages are rendered.
+   */
+  readonly languages?: {
+    /**
+     * Whether the named language will be rendered or not.
+     *
+     * @default false
+     */
+    readonly [name: string]: boolean | undefined;
+  };
 }
 
 export type CatalogBuilderInput = Pick<StateMachineInput, 'package'> & { readonly startAfter?: string };
