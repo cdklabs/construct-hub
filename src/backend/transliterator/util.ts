1646306469702aab78b842450327d06f7cf64c4a
Christopher Rybicki <rybickic@amazon.com>
2021-10-27 12:14:01 -0700

    Update src/backend/transliterator/util.ts
    Co-authored-by: Romain Marcadier <romain.muller@telecomnancy.net>

--- src/backend/transliterator/util.ts
+++ src/backend/transliterator/util.ts
@@ -1,13 +1,14 @@
 import * as fs from 'fs';
 import * as internal from 'stream';
 
-export async function writeFile(filePath: string, readStream: internal.Readable): Promise<boolean> {
-  return new Promise((ok, ko) => {
+export async function writeFile(filePath: string, readStream: internal.Readable): Promise<void> {
+  return new Promise<void>((ok, ko) => {
     const writeStream = fs.createWriteStream(filePath);
     readStream.pipe(writeStream);
-    writeStream.on('error', ko);
-    writeStream.on('finish', () => {
-      ok(true);
+    readStream.once('error', ko);
+    writeStream.once('error', ko);
+    writeStream.once('finish', () => {
+      ok();
     });
   });
 }
