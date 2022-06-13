ccebb16bb831515fa78f69ac36b6d98e442d62f5
Christopher Rybicki <rybickic@amazon.com>
2022-06-13 10:41:59 -0700

    Remove darkMode parameter since the SVG will be dynamically themed

--- src/webapp/badge-redirect-function/redirect-function.ts
+++ src/webapp/badge-redirect-function/redirect-function.ts
@@ -1,18 +1,8 @@
 // @ts-ignore
 function handler(event: any) {
-  var request = event.request;
-
-  if (request.querystring.darkMode) {
-    return {
-      statusCode: 302,
-      statusDescription: 'Found',
-      headers: { location: { value: '/badge-dark.svg' } },
-    };
-  } else {
-    return {
-      statusCode: 302,
-      statusDescription: 'Found',
-      headers: { location: { value: '/badge.svg' } },
-    };
-  }
+  return {
+    statusCode: 302,
+    statusDescription: 'Found',
+    headers: { location: { value: '/badge-dynamic.svg' } },
+  };
 }
