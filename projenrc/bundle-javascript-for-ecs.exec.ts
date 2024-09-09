75a1a079289a49e4ae1d300d86b546e2b69cd432
Momo Kornher <kornherm@amazon.co.uk>
2024-09-09 13:08:54 +0100

    chore: print the bundle size of ecs tasks
    This was lost when we moved to a custom bundler.
    Manually recreates the output format that esbuild uses.

--- projenrc/bundle-javascript-for-ecs.exec.ts
+++ projenrc/bundle-javascript-for-ecs.exec.ts
@@ -1,4 +1,10 @@
+import { basename, dirname, posix } from 'path';
+import { Instance as Chalk } from 'chalk';
 import * as esbuild from 'esbuild';
+
+const chalk = new Chalk({
+  level: process.env.NO_COLOR ? 0 : 1,
+});
 
 async function main(args: string[]) {
   const [entrypoint, outfile, maybeWatch] = args;
@@ -53,10 +59,44 @@ async function main(args: string[]) {
           `Bundle built from '${entrypoint}' contains 'aws-cdk-lib' somewhere in its dependency closure. Shake it!`
         );
       }
+
+      if (result.metafile) {
+        console.log('\n' + formatOutputs(result.metafile).join('\n') + '\n');
+      }
     }
   } finally {
     await context.dispose();
   }
+}
+
+function formatOutputs(metafile: esbuild.Metafile): string[] {
+  const files = new Array<[string, string]>();
+  for (const output of Object.entries(metafile.outputs)) {
+    files.push([output[0], formatBytes(output[1].bytes)]);
+  }
+
+  // get the length of the longest file
+  const max = files.reduce((a, b) => (a > b[0].length ? a : b[0].length), 0);
+
+  return files
+    .sort((a, b) => a[0].localeCompare(b[0]))
+    .map(
+      (output) =>
+        `  ${formatPath(output[0])}${' '.repeat(
+          max - output[0].length + 2
+        )}${chalk.cyan(output[1])}`
+    );
+}
+
+function formatPath(path: string): string {
+  return `${dirname(path)}${posix.sep}${chalk.bold(basename(path))}`;
+}
+
+function formatBytes(bytes: number): string {
+  const k = bytes > 0 ? Math.floor(Math.log2(bytes) / 10) : 0;
+  const rank = ['b', 'kb', 'mb', 'gb', 'tb'][k];
+  const value = bytes / Math.pow(1000, k);
+  return value.toFixed(1) + rank;
 }
 
 function ignoreWarnings(
