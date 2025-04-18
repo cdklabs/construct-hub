b7a1fd9afa4b9765d832a8c6607bbbd87962de6a
Kaizen Conroy <36202692+kaizencc@users.noreply.github.com>
2025-04-18 15:45:58 -0400

    Update src/package-sources/npmjs/npm-js-follower.lambda.ts

--- src/package-sources/npmjs/npm-js-follower.lambda.ts
+++ src/package-sources/npmjs/npm-js-follower.lambda.ts
@@ -86,7 +86,7 @@ export async function handler(event: ScheduledEvent, context: Context) {
   const knownVersions = await loadLastKnownVersions(stagingBucket);
 
   // The last written marker seq id.
-  let updatedMarker: string | number = 62928688; // initialMarker;
+  let updatedMarker = initialMarker;
 
   // The slowest batch processing time so far (starts at 30 seconds). This is how much time should
   // be left before timeout if a new batch is to be fetched.
